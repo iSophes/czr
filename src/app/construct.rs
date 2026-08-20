122fcdeaa20e43e3e9fda85cc9870e0f653d44cd
Sophie <87262182+iSophes@users.noreply.github.com>
2026-08-20 21:37:44 +0100

    :sparkles: add short and long descriptions

--- src/app/construct.rs
+++ src/app/construct.rs
@@ -1,10 +1,9 @@
 use std::vec;
 
-use clap::builder::Str;
 use dialoguer::FuzzySelect;
 use owo_colors::OwoColorize;
 
-use crate::app::app_config;
+use crate::{app::app_config, util::util::display_error};
 
 pub async fn get_commit_type(settings: app_config::Settings) -> String {
     let mut item_strings = vec![] as vec::Vec<String>;
@@ -66,8 +65,46 @@ pub async fn get_commit_type(settings: app_config::Settings) -> String {
         .to_owned();
 }
 
+async fn get_short_description() -> String {
+    loop {
+        let short: String = dialoguer::Input::new()
+            .with_prompt(&format!(
+                "{question} {text} {chars}",
+                question = "?".bright_green().bold(),
+                text = "Write a short description".bold().white(),
+                chars = "(Max 75 characters.)".default_color()
+            ))
+            .interact_text()
+            .unwrap();
+
+        if short.len() > 75 {
+            display_error(&format!(
+                "Short description too long! You're at {chars} characters with that.",
+                chars = short.len()
+            ));
+            continue;
+        }
+
+        return short;
+    }
+}
+
+async fn get_long_description() -> String {
+    let long: String = dialoguer::Input::new()
+        .with_prompt(&format!(
+            "{question} {text}",
+            question = "?".bright_green().bold(),
+            text = "Write a longer description".bold().white(),
+        ))
+        .interact_text()
+        .unwrap();
+
+    return long;
+}
+
 pub async fn construct_message(settings: app_config::Settings) -> bool {
     let commit_type = get_commit_type(settings).await;
-
+    let short_description = get_short_description().await;
+    let long_description = get_long_description().await;
     return true;
 }
